d966c4af7b9a42cbb63057c40e8ee71b018e8641
Kadir Turan <kadirrturan@gmail.com>
2022-03-24 14:43:59 +0300

    Added Sample book repository queries
    Added Sample book repository queries

--- pkg/store/book/repo.go
+++ pkg/store/book/repo.go
@@ -12,3 +12,24 @@ func NewBookRepository(db *gorm.DB) *BookRepository {
 func (b *BookRepository) Migrations() {
 	b.db.AutoMigrate(&Book{})
 }
+
+//FindAll Get all records <SELECT * FROM Books>
+func (b *BookRepository) FindAll() []Book {
+	var books []Book
+	b.db.Find(&books)
+	return books
+}
+
+//FindById Get By Id <SELECT * FROM Books WHERE ID = id>
+func (b *BookRepository) FindById(id int) Book {
+	var book Book
+	b.db.Where(`"ID" = ?`, id).Find(&book)
+	return book
+}
+
+//FindByAuthorID Get by Author ID <SELECT * FROM Books WHERE AuthorID = authorID ORDER BY ID DESC>
+func (b *BookRepository) FindByAuthorID(authorID int) []Book {
+	var books []Book
+	b.db.Where(`"AuthorID" = ?`, authorID).Order("Id desc").Find(&books)
+	return books
+}
